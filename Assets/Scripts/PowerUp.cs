a015dd633720a61a166a4b9ac68be63b598bc782
Jan-Gustav Liedtke <jan-gustav.liedtke@stud.hshl.de>
2021-10-09 14:24:29 +0200

    .

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField] GameObject pickupEffect;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider2D player)
+    {
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        Debug.Log("picked up!");
+
+        SpriteRenderer playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
+        playerSpriteRenderer.color = Color.green;
+
+        Destroy(gameObject);
+    }
+}
