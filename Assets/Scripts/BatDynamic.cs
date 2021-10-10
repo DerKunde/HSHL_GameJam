9371e3b343613f283516234dd1a46fc05489abf4
Hallmann <michelle-celina.hallmann@stud.hshl.de>
2021-10-10 13:26:28 +0200

    bat dies now when charcter jumps on it

--- Assets/Scripts/BatDynamic.cs
+++ Assets/Scripts/BatDynamic.cs
@@ -30,6 +30,8 @@ public class BatDynamic : MonoBehaviour
     float distanceBatAndChar;
 
     private Vector3 startpoint;
+
+    private bool dead;
     
  
 
@@ -71,59 +73,78 @@ public class BatDynamic : MonoBehaviour
     {
         if (collision.gameObject.name == "Character")
         {
-            collision.gameObject.GetComponent<GetGameManager>().gameManager.GetDamage();
-            rb.AddForce(force*(-20));
-            Debug.Log(force);
+            if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y < 0)
+            {
+                dead = true;
+                CancelInvoke("UpdatePath");
+                attacking = false;
+                GetComponent<Collider2D>().enabled = false;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -5f);
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 30, ForceMode2D.Impulse);
+                Destroy(gameObject, 1);
+            }
+            else
+            {
+                collision.gameObject.GetComponent<GetGameManager>().gameManager.GetDamage();
+                rb.AddForce(force * (-20));
+            }
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        if(attacking && !attack(target, transform) && (Vector2.Distance(startpoint, transform.position) < 2))
+        if (!dead)
         {
-            Debug.Log("Invokes get cancelled");
-            CancelInvoke("UpdatePath");
-            attacking = false;
+            if (attacking && !attack(target, transform) && (Vector2.Distance(startpoint, transform.position) < 2))
+            {
+                Debug.Log("Invokes get cancelled");
+                CancelInvoke("UpdatePath");
+                attacking = false;
+            }
+
+            if (!attacking && attack(target, transform))
+            {
+                attacking = true;
+                InvokeRepeating("UpdatePath", 0f, .5f);
+            }
+
+            if (path == null) return;
+
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                reachedEndOfPath = true;
+                return;
+            }
+            else
+            {
+                reachedEndOfPath = false;
+            }
+
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            force = direction * speed * Time.deltaTime;
+
+            rb.AddForce(force);
+
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+            if (distance < nextWaypointDistance)
+            {
+                currentWaypoint++;
+            }
+
+            if (rb.velocity.x >= 0.05f)
+            {
+                transform.localScale = new Vector3(-0.05f, 0.05f, 1);
+            }
+            else if (rb.velocity.x < -0.01f)
+            {
+                transform.localScale = new Vector3(0.05f, 0.05f, 1);
+            }
         }
-
-        if (!attacking && attack(target, transform))
+        else
         {
-            attacking = true;
-            InvokeRepeating("UpdatePath", 0f, .5f);
-        }
-
-        if (path == null) return;
-
-        if(currentWaypoint >= path.vectorPath.Count)
-        {
-            reachedEndOfPath = true;
-            return;
-        } else
-        {
-            reachedEndOfPath = false;
-        }
-
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        force = direction * speed * Time.deltaTime;
-
-        rb.AddForce(force);
-
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-
-        if (distance < nextWaypointDistance)
-        {
-            currentWaypoint++;
-        }
-
-        if (rb.velocity.x >= 0.05f)
-        {
-            transform.localScale = new Vector3(-0.05f, 0.05f, 1);
-        }
-        else if (rb.velocity.x < -0.01f)
-        {
-            transform.localScale = new Vector3(0.05f, 0.05f, 1);
+            transform.Rotate(0, 0, 1);
         }
     }
 
